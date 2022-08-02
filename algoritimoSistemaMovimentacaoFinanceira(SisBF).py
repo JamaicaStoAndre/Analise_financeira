2c9e0d045eff2617ad7ad5026cec1a5224590153
João Maykon Mendes <89603949+JamaicaStoAndre@users.noreply.github.com>
2022-08-02 10:44:58 -0300

    Add files via upload

---
+++ algoritimoSistemaMovimentacaoFinanceira(SisBF).py
@@ -0,0 +1,19 @@
+# 1 - BAIXAR EM ARQUIVOU O LISTA AS EMPRESAS B3
+    # 1.1 - Armazar em variável(lista, upper, retirar espaços)
+    #
+# 2 - ACESSAR SITE NA NET E BAIXAR NOTÍCIAS
+    # 2.1 - Listar o nome do site e seu xpath
+    # 2.2 - Colar as Notícias em str, separalas, upper, retirar espaços em uma lista (variável)
+    # 2.3 - Verificar se é palavras chaves positivas ou negativas;
+#
+# 3 - Localizar empresas listadas B3 e ver qual delas aparecem na ListaNEWS
+    # 3.1 - SE ENCONTRAR, ARMAZENAR EMPRESALOCALIZADAB3 EM VARIÁVEL
+        # 3.1.1 - VARIÁVEL EMPRESALOCALIZADAB3 =+[c] (c = contador')
+        # 3.1.2 - QTDVEZESEMPRESAB3FOILOCALIZADA =+ 1
+    # 3.2 - COMPARA AS PALAVRAS CHAVES POSITIVAS OU NEGATIVAS
+
+# 4 -  Comerçar a armazenar os valores da empresa localizada nos itens 1, 2 e 3.
+
+# 5 - CADASTRAR PALAVRAS CHAVES POSITIVAS
+
+# 6 - CADASTRAR PALAVRAS CHAVES NEGATIVAS
